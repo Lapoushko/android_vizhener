bd183fcd6bcb6b760852bd2fa6df127d3f745bc8
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-12-04 22:00:00 +0500

    fix

--- app/src/main/java/com/example/android_vizhener/mapper/util/TextValidator.kt
+++ app/src/main/java/com/example/android_vizhener/mapper/util/TextValidator.kt
@@ -8,8 +8,7 @@ import com.example.android_vizhener.util.Constants
 class TextValidator {
     private val letters = Constants.alph
     fun validate(input: String): String {
-        val alphabet = letters + letters.uppercase()
-
+        val alphabet = letters.uppercase()
         return input.filter { it in alphabet }.uppercase()
     }
 }
